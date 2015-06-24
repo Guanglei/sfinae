81002799824b0efd30b50fa149763dfc22ba1698
guanglei.fu <fuguanglei@gmail.com>
2015-06-24 21:30:41 +0800

    support checking interface

--- main.cpp
+++ main.cpp
@@ -16,11 +16,29 @@ struct InvokeUtil
     void invoke_foo_util(...)
     {
     }
-    
+
+    template<typename U>
+    bool has_foo_util(U& u, invoke_foo_checker<U, &U::foo>*)
+    {
+        return true;
+    }
+
+    template<typename U>
+    bool has_foo_util(...)
+    {
+        return false;
+    }
+
     template<typename T>
     void invoke_foo(T& t, int i)
     {
         invoke_foo_util<T>(t, i, 0);
+    }
+
+    template<typename T>
+    bool has_foo(T& t)
+    {
+        return has_foo_util<T>(t, 0);
     }
     
 };
@@ -45,6 +63,16 @@ int main()
     
     iu.invoke_foo(f1, 42);
     iu.invoke_foo(f2, 42);
+    
+    if(iu.has_foo(f1))
+    {
+        std::cout << "Foo1 has foo" << std::endl;
+    }
+    
+    if(iu.has_foo(f2))
+    {
+        std::cout << "Foo2 has foo" << std::endl;
+    }
 
     return 0;
 }   
